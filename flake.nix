3b4bc8a7a249bf02e66f0d0f7db12dce5796b153
Ole Krüger <ole.kruger@trili.tech>
2023-04-14 12:13:19 +0100

    Bring in Rust overlay

--- flake.nix
+++ flake.nix
@@ -4,25 +4,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        inputs',
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
+
         devShells.default = pkgs.mkShell {
           name = "dev";
-          packages = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            rust-analyzer
-            libiconv
-          ];
+          packages = with pkgs;
+            [
+              rust-bin.stable.latest.default
+              rust-analyzer
+            ]
+            ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+              # CoreFoundation
+            ]);
         };
       };
     };
