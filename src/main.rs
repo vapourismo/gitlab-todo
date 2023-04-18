7295ac5f8b11af52b988ce2f08ded8a59cca2476
Ole Krüger <ole.kruger@trili.tech>
2023-04-18 11:44:18 +0100

    Embed methods into struct impls

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,15 @@ use std::{
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-struct UserInfo {
+struct User {
   id: usize,
   name: String,
   username: String,
 }
 
-impl UserInfo {
+impl User {
   fn get<UserName: AsRef<str>>(client: &Client, user: UserName) -> Result<Self> {
-    let response: Vec<UserInfo> = client
+    let response: Vec<User> = client
       .get("https://gitlab.com/api/v4/users")
       .query(&[("username", user.as_ref())])
       .send()?
@@ -36,6 +36,91 @@ impl UserInfo {
       .into_iter()
       .last()
       .ok_or("No user found with that name".into())
+  }
+
+  fn get_recent_pushes(&self, client: &Client) -> Result<Vec<RecentPush>> {
+    Ok(
+      client
+        .get(format!(
+          "https://gitlab.com/api/v4/users/{}/events",
+          self.id
+        ))
+        .query(&[("action", "pushed")])
+        .send()?
+        .json()?,
+    )
+  }
+
+  fn get_mrs_to_review(&self, client: &Client) -> Result<HashMap<usize, MergeRequest>> {
+    let mrs: Vec<MergeRequest> = client
+      .get("https://gitlab.com/api/v4/merge_requests")
+      .query(&[
+        ("state", "opened"),
+        ("scope", "all"),
+        ("reviewer_username", self.username.as_str()),
+      ])
+      .send()?
+      .json()?;
+    let now = Utc::now();
+    let mrs = mrs
+      .into_iter()
+      .filter(|mr| now.sub(mr.updated_at).num_days() <= 14)
+      .map(|mr| (mr.id, mr))
+      .collect();
+    Ok(mrs)
+  }
+
+  fn get_assigned_mrs(&self, client: &Client) -> Result<HashMap<usize, MergeRequest>> {
+    let mrs: Vec<MergeRequest> = client
+      .get("https://gitlab.com/api/v4/merge_requests")
+      .query(&[
+        ("state", "opened"),
+        ("scope", "all"),
+        ("assignee_username", self.username.as_str()),
+      ])
+      .send()?
+      .json()?;
+    let mrs = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
+    Ok(mrs)
+  }
+
+  fn get_authored_mrs(&self, client: &Client) -> Result<HashMap<usize, MergeRequest>> {
+    let mrs: Vec<MergeRequest> = client
+      .get("https://gitlab.com/api/v4/merge_requests")
+      .query(&[
+        ("state", "opened"),
+        ("scope", "all"),
+        ("author_username", self.username.as_str()),
+      ])
+      .send()?
+      .json()?;
+    let mrs = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
+    Ok(mrs)
+  }
+
+  fn get_related_mrs(&self, client: &Client) -> Result<HashMap<usize, MergeRequest>> {
+    let recent_mrs: HashMap<usize, MergeRequest> = self
+      .get_recent_pushes(&client)?
+      .iter()
+      .map(|recent_push| {
+        MergeRequest::get_by_branch(&client, recent_push.project_id, &recent_push.push_data.ref_)
+      })
+      .collect::<Result<Vec<_>>>()?
+      .into_iter()
+      .flat_map(|mrs| mrs.into_iter())
+      .collect();
+    let to_review = self.get_mrs_to_review(&client)?;
+    let assigned = self.get_assigned_mrs(&client)?;
+    let authored = self.get_authored_mrs(&client)?;
+
+    let all_mrs: HashMap<usize, MergeRequest> = recent_mrs
+      .into_iter()
+      .chain(to_review)
+      .chain(assigned)
+      .chain(authored)
+      .collect();
+
+    Ok(all_mrs)
   }
 }
 
@@ -49,18 +134,6 @@ struct PushData {
 struct RecentPush {
   project_id: usize,
   push_data: PushData,
-}
-
-fn recent_pushes(client: &Client, user: &UserInfo) -> Result<Vec<RecentPush>> {
-  let pushes = client
-    .get(format!(
-      "https://gitlab.com/api/v4/users/{}/events",
-      user.id
-    ))
-    .query(&[("action", "pushed")])
-    .send()?
-    .json()?;
-  Ok(pushes)
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
@@ -85,109 +158,37 @@ struct MergeRequest {
   references: References,
   web_url: String,
   updated_at: DateTime<Utc>,
-  author: UserInfo,
-  assignees: Vec<UserInfo>,
-  reviewers: Vec<UserInfo>,
+  author: User,
+  assignees: Vec<User>,
+  reviewers: Vec<User>,
 }
 
-fn reviewing(client: &Client, user: &str) -> Result<HashMap<usize, MergeRequest>> {
-  let mrs: Vec<MergeRequest> = client
-    .get("https://gitlab.com/api/v4/merge_requests")
-    .query(&[
-      ("state", "opened"),
-      ("scope", "all"),
-      ("reviewer_username", user),
-    ])
-    .send()?
-    .json()?;
-  let now = Utc::now();
-  let mrs = mrs
-    .into_iter()
-    .filter(|mr| now.sub(mr.updated_at).num_days() <= 14)
-    .map(|mr| (mr.id, mr))
-    .collect();
-  Ok(mrs)
-}
-
-fn assigned(client: &Client, user: &str) -> Result<HashMap<usize, MergeRequest>> {
-  let mrs: Vec<MergeRequest> = client
-    .get("https://gitlab.com/api/v4/merge_requests")
-    .query(&[
-      ("state", "opened"),
-      ("scope", "all"),
-      ("assignee_username", user),
-    ])
-    .send()?
-    .json()?;
-  let mrs = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
-  Ok(mrs)
-}
-fn authored(client: &Client, user: &str) -> Result<HashMap<usize, MergeRequest>> {
-  let mrs: Vec<MergeRequest> = client
-    .get("https://gitlab.com/api/v4/merge_requests")
-    .query(&[
-      ("state", "opened"),
-      ("scope", "all"),
-      ("author_username", user),
-    ])
-    .send()?
-    .json()?;
-  let mrs = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
-  Ok(mrs)
-}
-
-fn mrs_for_branch(
-  client: &Client,
-  project_id: usize,
-  branch: &str,
-) -> Result<HashMap<usize, MergeRequest>> {
-  let mrs: Vec<MergeRequest> = client
-    .get(format!(
-      "https://gitlab.com/api/v4/projects/{}/merge_requests",
-      project_id
-    ))
-    .query(&[
-      ("state", "opened"),
-      ("scope", "all"),
-      ("source_branch", branch),
-    ])
-    .send()?
-    .json()?;
-  let mrs: HashMap<usize, MergeRequest> = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
-  Ok(mrs)
-}
-
-fn all_mrs(client: &Client, user: &UserInfo) -> Result<HashMap<usize, MergeRequest>> {
-  let recent_mrs: HashMap<usize, MergeRequest> = recent_pushes(&client, &user)?
-    .iter()
-    .map(|recent_push| {
-      mrs_for_branch(
-        &client,
-        recent_push.project_id,
-        recent_push.push_data.ref_.as_str(),
-      )
-    })
-    .collect::<Result<Vec<_>>>()?
-    .into_iter()
-    .flat_map(|mrs| mrs.into_iter())
-    .collect();
-  let to_review = reviewing(&client, user.username.as_str())?;
-  let assigned = assigned(&client, user.username.as_str())?;
-  let authored = authored(&client, user.username.as_str())?;
-
-  let all_mrs: HashMap<usize, MergeRequest> = recent_mrs
-    .into_iter()
-    .chain(to_review)
-    .chain(assigned)
-    .chain(authored)
-    .collect();
-
-  Ok(all_mrs)
+impl MergeRequest {
+  fn get_by_branch<BranchName: AsRef<str>>(
+    client: &Client,
+    project_id: usize,
+    branch: BranchName,
+  ) -> Result<HashMap<usize, MergeRequest>> {
+    let mrs: Vec<MergeRequest> = client
+      .get(format!(
+        "https://gitlab.com/api/v4/projects/{}/merge_requests",
+        project_id
+      ))
+      .query(&[
+        ("state", "opened"),
+        ("scope", "all"),
+        ("source_branch", branch.as_ref()),
+      ])
+      .send()?
+      .json()?;
+    let mrs: HashMap<usize, MergeRequest> = mrs.into_iter().map(|mr| (mr.id, mr)).collect();
+    Ok(mrs)
+  }
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 struct Approver {
-  user: UserInfo,
+  user: User,
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
@@ -196,22 +197,24 @@ struct ApprovalInfo {
   approved_by: Vec<Approver>,
 }
 
-fn approval_info(client: &Client, mr: &MergeRequest) -> Result<ApprovalInfo> {
-  let info = client
-    .get(format!(
-      "https://gitlab.com/api/v4/projects/{}/merge_requests/{}/approvals",
-      mr.project_id, mr.iid
-    ))
-    .send()?
-    .json()?;
-  Ok(info)
+impl ApprovalInfo {
+  fn get(client: &Client, mr: &MergeRequest) -> Result<Self> {
+    let info = client
+      .get(format!(
+        "https://gitlab.com/api/v4/projects/{}/merge_requests/{}/approvals",
+        mr.project_id, mr.iid
+      ))
+      .send()?
+      .json()?;
+    Ok(info)
+  }
 }
 
 fn make_link(url: &str, title: &str) -> String {
   format!("\x1B]8;;{}\x1B\\{}\x1B]8;;\x1B\\", url, title)
 }
 
-fn priority(mr: &MergeRequest, approval_info: &ApprovalInfo, user: &UserInfo) -> isize {
+fn priority(mr: &MergeRequest, approval_info: &ApprovalInfo, user: &User) -> isize {
   let mut prio = 0;
 
   if mr.assignees.iter().any(|assignee| assignee.id == user.id) {
@@ -260,16 +263,16 @@ fn cell(width: usize, body: &str) -> String {
   }
 }
 
-fn print_all(client: &Client, user_info: &UserInfo) -> Result<()> {
-  let all_mrs: HashMap<usize, MergeRequest> = all_mrs(&client, &user_info)?;
+fn print_all(client: &Client, user: &User) -> Result<()> {
+  let all_mrs: HashMap<usize, MergeRequest> = user.get_related_mrs(&client)?;
   let mut all_mrs: Vec<(MergeRequest, ApprovalInfo)> = all_mrs
     .into_iter()
-    .map(|(_, mr)| approval_info(&client, &mr).map(|approval_info| (mr, approval_info)))
+    .map(|(_, mr)| ApprovalInfo::get(&client, &mr).map(|approval_info| (mr, approval_info)))
     .collect::<Result<_>>()?;
 
   all_mrs.sort_by(|lhs, rhs| {
-    let lhs_prio = priority(&lhs.0, &lhs.1, &user_info);
-    let rhs_prio = priority(&rhs.0, &rhs.1, &user_info);
+    let lhs_prio = priority(&lhs.0, &lhs.1, &user);
+    let rhs_prio = priority(&rhs.0, &rhs.1, &user);
     lhs_prio.cmp(&rhs_prio).reverse()
   });
 
@@ -291,11 +294,7 @@ fn print_all(client: &Client, user_info: &UserInfo) -> Result<()> {
   for (mr, approval_info) in all_mrs {
     let reference = make_link(&mr.web_url, &cell(ref_width, &mr.references.full)).blue();
     let title = cell(title_width, &mr.title).with(
-      if mr
-        .assignees
-        .iter()
-        .any(|assignee| assignee.id == user_info.id)
-      {
+      if mr.assignees.iter().any(|assignee| assignee.id == user.id) {
         Color::Red
       } else if approval_info.approvals_left < 1 {
         Color::Green
@@ -354,10 +353,10 @@ fn main() -> Result<()> {
     .nth(1)
     .ok_or::<Box<dyn Error>>("First argument should be the GitLab user name".into())?;
 
-  let user_info = UserInfo::get(&client, user_name.as_str())?;
+  let user = User::get(&client, user_name.as_str())?;
 
   loop {
-    print_all(&client, &user_info)?;
+    print_all(&client, &user)?;
     sleep(Duration::from_secs(30));
   }
 }
